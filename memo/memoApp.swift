629a012833176f8eedacd1daf94da11eadef8939
mingeunqq <whalsrms111@naver.com>
2023-05-02 14:38:16 +0900

    Initial Commit

---
+++ memo/memoApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoApp.swift
+//  memo
+//
+//  Created by 민근의 mac on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct memoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
